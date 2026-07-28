0f604fd0901ebfc87d99ecb65eabad72519c4e21
ananyakadam28 <ananyakadam0028@gmail.com>
2026-07-29 01:25:51 +0530

    Update frontend API URL for Render deployment

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://contact-hub-j1g5.onrender.com";
 
 // ========================================================================= //
 // INITIAL / DEFAULT DATA                                                    //
