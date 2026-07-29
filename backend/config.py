c9236d2821dfff15cc987764f839711dd6def7bc
Tanvi Patil <tanvip856@gmail.com>
2026-07-29 17:27:31 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "contact_manager")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "contacts")
